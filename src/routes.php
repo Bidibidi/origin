9cccfe468860284bf5f47a835a3f5291ae968427
Bidibidi <bidibidi.dev@gmail.com>
2018-08-13 08:09:51 +0300

    Fixed Auth
    Auth fixed for user

--- src/routes.php
+++ src/routes.php
@@ -7,12 +7,16 @@ Descriptions    :   Routes for pages. You can add or change path your pages.
 */
 
 if ( (config('bidibidi-origin-config.origin-pages-active')) == '1' )  {
+
+Route::group(['middleware' => ['web']], function(){
 // Active or passive this settings in config pages. 
-Route::get(config('bidibidi-origin-config.route-index'),   'Bidibidi\Origin\Controllers\BidibidiOriginController@startPage')->name('index');
-Route::get(config('bidibidi-origin-config.route-aboutus'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@aboutus')->name('aboutus');
-Route::get(config('bidibidi-origin-config.route-contact'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@contact')->name('contact');
-Route::get(config('bidibidi-origin-config.route-sitemap'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@sitemap')->name('sitemap');
-Route::get(config('bidibidi-origin-config.route-faq'),	   'Bidibidi\Origin\Controllers\BidibidiOriginController@faq')->name('faq');
-Route::get(config('bidibidi-origin-config.route-privacy'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@privacy')->name('privacy');
-Route::get(config('bidibidi-origin-config.route-terms'),   'Bidibidi\Origin\Controllers\BidibidiOriginController@terms')->name('terms');
+	Route::get(config('bidibidi-origin-config.route-index'),   'Bidibidi\Origin\Controllers\BidibidiOriginController@startPage')->name('index');
+	Route::get(config('bidibidi-origin-config.route-aboutus'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@aboutus')->name('aboutus');
+	Route::get(config('bidibidi-origin-config.route-contact'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@contact')->name('contact');
+	Route::get(config('bidibidi-origin-config.route-sitemap'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@sitemap')->name('sitemap');
+	Route::get(config('bidibidi-origin-config.route-faq'),	   'Bidibidi\Origin\Controllers\BidibidiOriginController@faq')->name('faq');
+	Route::get(config('bidibidi-origin-config.route-privacy'), 'Bidibidi\Origin\Controllers\BidibidiOriginController@privacy')->name('privacy');
+	Route::get(config('bidibidi-origin-config.route-terms'),   'Bidibidi\Origin\Controllers\BidibidiOriginController@terms')->name('terms');
+});
+
 }
